c518b6178a0d4aaa4c33e1c8dc2db4c7f3694707
Rakib ali <21pa1a1273@vishnu.edu.in>
2024-07-23 16:08:28 +0530

    courses added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='bg-blue-600 font-bold text-8xl text-white'> hii</h1>
+     
     </div>
   );
 }
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -2,7 +2,12 @@ const Courses = () =>{
     return(
         <>
            <div>
-             
+             <div>
+                <h1 className="flex justify-center items-center font-bold text-5xl">TOP COURSES</h1>
+             </div>
+             <div>
+        
+             </div>
            </div>
         </>
     )
