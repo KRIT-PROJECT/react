51be9f526e66c0a5e387b6ec1dcf042a6fd2024d
Rakib ali <21pa1a1273@vishnu.edu.in>
2024-07-23 15:59:24 +0530

    courses added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-blue-600 font-bold text-8xl text-white'> hii</h1>
+    </div>
+  );
+}
+
+export default App;
