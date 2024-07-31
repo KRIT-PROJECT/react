59885399a7b120795d68acba0ce6ea0f2e817427
PallaviPenta <21pa1a1291@vishnu.edu.in>
2024-07-31 14:58:16 +0530

    demos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Courses from './components/Courses';
+import Demos from './components/Demos';
 function App() {
   return (
     <div className="App bg-[#0A0929] h-screen">
       <Courses />
+      <Demos/>
     </div>
   );
 }
