3971770719ab1ee07783ba7a402771dc06961085
POTHULA SANJAY KUMAR <21pa1a1299@vishnu.edu.in>
2024-07-31 14:52:27 +0530

    sanjay

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Courses from './components/Courses';
+import Footer from './components/Footer';
+import Newsletter from './components/Newsletter';
 function App() {
   return (
     <div className="App bg-[#0A0929] h-screen">
       <Courses />
+      <Newsletter/>
+      <Footer/>
     </div>
   );
 }
