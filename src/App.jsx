b84f14e28ac18914f53eca62c38bf010c0d8b283
POTHULA SANJAY KUMAR <21pa1a1299@vishnu.edu.in>
2024-08-02 11:35:59 +0530

    contact & Feature are added by sanjay

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+// import './App.css';
+
+// import Body from './components/Body';
+// import CourseContent from './components/CourseContent';
+// import Footer from './components/Footer';
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import Features from './components/Features'
+// import Contact from './components/Contact';
+
+
+
+// function App() {
+//   // const [body,setBody] = useState(false);
+//   return (
+//     <BrowserRouter>
+//       <div className="App bg-[#f7f7f7] h-screen">
+//         <Routes>
+//           <Route path="/" element={<Body />} />
+//           <Route path='/info' element={<CourseContent />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </BrowserRouter>
+
+//   );
+// }
+
+// export default App;
+
+
+
+
 import './App.css';
 
 import Body from './components/Body';
@@ -5,21 +38,24 @@ import CourseContent from './components/CourseContent';
 import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Features from './components/Features';
+import Contact from './components/Contact';
 
 function App() {
-  // const [body,setBody] = useState(false);
   return (
     <BrowserRouter>
       <div className="App bg-[#f7f7f7] h-screen">
         <Routes>
           <Route path="/" element={<Body />} />
-          <Route path='/info' element={<CourseContent />} />
+          <Route path="/info" element={<CourseContent />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </div>
     </BrowserRouter>
-
   );
 }
 
 export default App;
+
